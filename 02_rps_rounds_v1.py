167ec9f406e8a99485fa0b4b49f1ecb524bc4119
DaxtorT <110640402+DaxtorT@users.noreply.github.com>
2023-03-21 13:30:40 +1300

    Add files via upload

--- 02_rps_rounds_v1.py
+++ 02_rps_rounds_v1.py
@@ -1,12 +1,52 @@
+def choice_checker(question, error, valid_list):
+    valid = False
+    while not valid:
+        # Ask user for choice (and force lowercase)
+        response = input(question).lower()
+
+        # Runs through list and if response is an item in list (or first letter), the full name is returned
+        for item in valid_list:
+            if response == item[0] or response == item:
+                return item
+
+        # Output error if response not in list        
+        print(error)
+        print()
+
+def num_checker(question, min_value, max_value):
+    keep_going = ""
+    while keep_going == "":
+        try:
+            # Ask the question
+            response = int(input(question))
+
+            # If the amount is too low / too high give
+            if  min_value < response <= max_value:
+                return response
+
+            # Output error
+            elif response < min_value + 1:
+                print("error_1")
+
+            elif response > max_value:
+                print("error_2")
+        
+        except ValueError:
+            print("error_3")
 
 # Main Routine goes here
 # Set-Up or Constants go here
 rounds_played = 0
 
+mode_choice_list = ["infinite", "number"]
+
 choose_instruction = "Please choose rock (r), paper (p) or scissors (s)"
 
 # Ask number or infinite
-num_inf = input("Do you want to play a 'Number' or rounds or 'Infinte'? ").lower()
+num_inf = choice_checker("Do you want to play a 'Number' or rounds or 'Infinte'? ", 
+                         "Please choose infinite / number",mode_choice_list)
+print(f'you chose {num_inf}')
+
 if num_inf == "number":
     # Ask how many rounds
     response = int(input("How many rounds do you want to play? "))
