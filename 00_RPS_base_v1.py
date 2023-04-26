7cb1aafbc9b317b020371132ed119c05742d66d3
DaxtorT <110640402+DaxtorT@users.noreply.github.com>
2023-04-26 12:06:22 +1200

    Add files via upload

--- 00_RPS_base_v1.py
+++ 00_RPS_base_v1.py
@@ -38,6 +38,30 @@ def check_rounds():
 
         return response
 
+def statement_deco(deco_sides, deco_top_bottom, start_statement, three_line):
+    # Sets up decoration for sides of statement and top / bottom
+    sides = deco_sides * 3
+    single_statement = f"{sides} {start_statement} {sides}"
+
+    top_bottom = deco_top_bottom * len(single_statement)
+    big_statement = f"{top_bottom}\n{single_statement}\n{top_bottom}"
+
+    # Outputs either single or triple line statement
+    if three_line == 1:
+        return single_statement
+
+    elif three_line ==3:
+        return big_statement
+
+def instructions():
+    intro = statement_deco("*", "-", "How To Play", 1)
+    print(intro)
+    print("Playing Rock, Paper, Scissors is very simple")
+    print("All you have to do is choose an amount of rounds to play, or press enter to play continously")
+    print("Then you need to choose either Rock(R), Paper(P), or Scissors(S) to play for each round.")
+    print("If you chose 'Continous Mode' you will need to type 'xxx' to stop the rounds and finish the game")
+    return ""
+
 # Main Routine goes here
 # Constants for program
 round_no = 1
@@ -49,7 +73,6 @@ yes_no_instructions = "Have you played before? "
 yes_no_error = "Please say 'Yes' or 'No'" 
 choose_instruction = "What move do you want to make (Rock, Paper, Scissors)? "
 choose_error = "Please choose Rock, Paper or Scissors"
-instructions = "These will be instructions"
 
 # Lists or Dicts of valid responses
 yes_no_list = ["yes", "no"]
@@ -69,12 +92,19 @@ comparison_dict = {
 'p': ('paper'),
 's': ('scissors')}
 
+# Welcome to the game statement
+welcome_statement = statement_deco("=", "-", "Welcome to The Rock, Paper, Scissors Game", 3)
+print(welcome_statement)
+print()
+
 # Ask user if they have played before
-# If 'yes', show instructions
 played_before = choice_checker(yes_no_instructions, yes_no_error, yes_no_list)
 
-if played_before == "n":
-    print(instructions)
+# If 'yes', show instructions
+if played_before == "n" or played_before == "no":
+    print()
+    game_instructions = instructions()
+    print(game_instructions)
     print()
 
 else:
@@ -82,23 +112,26 @@ else:
 
 # Ask user for # of rounds, <enter> for infinite mode
 rounds = check_rounds()
+print()
 
 end_game = "no"
 while end_game == "no":
 
     # Rounds Heading
-    print()
     if rounds == "":
         heading = f"Continous Mode: Round {rounds_played + 1}"
+        print_heading = statement_deco("=", "-", heading, 1)
         
     else:
         heading = f"Round {rounds_played + 1} of {rounds}"
+        print_heading = statement_deco("-", "-", heading, 1)
         
-    print(heading)
+    print(print_heading)
 
     # Ask user to choose a move to make (Rock, Paper, Scissors)
     choose = choice_checker(choose_instruction, choose_error, rps_list)
-
+    print()
+    
     # End game if exit code is typed
     if choose == "xxx":
         break
@@ -145,22 +178,18 @@ while end_game == "no":
         rounds_drawn += 1
 
     elif result == "lost":
-        result = "You Lose"
+        result = ":( You Lose ):"
         rounds_lost += 1
 
     else:
-        result = "You WIN"
+        result = "! You WIN !"
 
     # Prints result of the current round
     print(f"Result: {result}")
+    print()
 
 # Quick winning rounds calculation
 rounds_won = rounds_played - rounds_lost - rounds_drawn
-
-# Shows user total round stats
-print()
-print("Rounds Summary")
-print(f"Won: {rounds_won}, Drawn: {rounds_drawn}, Lost: {rounds_lost}")
 
 # Calculate all game history
 percent_won = rounds_won / rounds_played * 100
@@ -174,13 +203,15 @@ show_history = choice_checker("Do you want to see your game history? ", "Please 
 
 if show_history == "yes" or show_history == "y":
     print()
-    print("Game History")
+    game_his = statement_deco("*", "-", "Game History", 1)
+    print(game_his)
     for game in game_summary:
         print(f"R{round_no}: {game}")
         round_no += 1
 
     print()
-    print("Game Statistics")
+    game_stat = statement_deco("*", "-", "Game Statistics", 1)
+    print(game_stat)
     print(f"Win: {rounds_won}, ({percent_won:.0f}%)",
         f"\nLoss: {rounds_lost}, ({percent_lost:.0f}%)",
         f"\nTie: {rounds_drawn}, ({percent_tie:.0f}%)")
@@ -190,4 +221,5 @@ else:
 
 # Final end game content
 print()
-print("Thanks for Playing")
+end_statement = statement_deco("!", "-", "Thanks for Playing", 3)
+print(end_statement)
